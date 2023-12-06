1f3a26cd892fc4a15064e39d3cb4faa4eac59ba2
colehardy <cole0hardy@gmail.com>
2023-12-06 09:49:23 -0700

    Navigation NO styling

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
-import {LocalizationProvider} from "@mui/x-date-pickers";
-import {AdapterDayjs} from "@mui/x-date-pickers/AdapterDayjs";
-import Form from './Form'
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+import { LocalizationProvider } from "@mui/x-date-pickers";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import Form from "./Form";
+import CalendarPage from "./CalendarPage";
+import Navbar from "./Navbar";
+import OldApp from "./OldApp";
 
 function App() {
-    return (
-        <LocalizationProvider dateAdapter={AdapterDayjs}>
-            <Form></Form>
-        </LocalizationProvider>
-    )
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<OldApp />} />
+        <Route path="/calendar" element={<CalendarPage />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
