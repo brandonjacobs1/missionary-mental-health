9b873a7c3673190d95c85ed0d7e9297da04461f8
Trevor_Lawson <trevor3lawson@gmail.com>
2023-12-06 10:02:25 -0700

    small fix

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import { LocalizationProvider } from "@mui/x-date-pickers";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import Form from "./Form";
-import Calendar from "./Calendar";
 
 function App() {
   return (
     <LocalizationProvider dateAdapter={AdapterDayjs}>
-      {/* <Form></Form> */}
-      <Calendar></Calendar>
+      <Form></Form>
     </LocalizationProvider>
   );
 }
