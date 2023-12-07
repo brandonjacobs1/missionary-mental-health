6457f6ee5486e808f43ec716366a4586c1d3ff30
Trevor_Lawson <trevor3lawson@gmail.com>
2023-12-07 10:21:40 -0700

    Calendar Page

--- src/CalendarPage.js
+++ src/CalendarPage.js
@@ -1,8 +1,17 @@
 // src/components/ContactPage.js
 import React from "react";
+import Calendar from "./Calendar";
+import { Grid } from "@mui/material";
 
 const CalendarPage = () => {
-  return <h1>CALENDAR</h1>;
+  return (
+    <>
+      <h1>CALENDAR</h1>
+      <Grid sx={{ pl: "10%", pr: "10%" }}>
+        <Calendar></Calendar>
+      </Grid>
+    </>
+  );
 };
 
 export default CalendarPage;
